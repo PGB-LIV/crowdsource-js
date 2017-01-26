c0c92f9ebd427f02f2537a62f3c0ba1206acade7
johnheap <johnheap@liv.ac.uk>
2017-01-26 15:48:05 +0000

    Lets try again

--- src/cs_worker.js
+++ src/cs_worker.js
@@ -1,193 +1,149 @@
 
-//JOHNS DEBUG MODERATOR
-	var DEBUG_LEVEL_OFF = 3;
-	var DEBUG_LEVEL_HIGH = 2; 
-	var DEBUG_LEVEL_MEDIUM = 1;
-	var DEBUG_LEVEL_LOW = 0;
 	
-	var debugMinimumLevel = DEBUG_LEVEL_MEDIUM;	//anything below this priority doesn't get output (set it to HIGH if only critical debug information)
-	function Debug(output , priority)	// = DEBUG_LEVEL_HIGH)
-	{
-		priority = (typeof priority !== 'undefined') ?  priority : DEBUG_LEVEL_HIGH;
-		if	(priority >= debugMinimumLevel)
-		{					
-			console.log(output);
-		}
-	}
-//END
-
+	var MAX_NUMBER_SCORES_RETURNED = 5;			//only retunr the top 5 peptide scores for this workunit
+	
+	
 	var WORKER_INITIALISING = 0;
-	var WORKER_AWAITING_SPECTRA = 1;	
-	var WORKER_AWAITING_PROTEIN = 2;
+	var WORKER_AWAITING_WORKUNIT = 1;	
 	var WORKER_PROCESSING = 3;
 	var WORKER_AWAITING_CONFIRMATION = 4;
 	var WORKER_COMPLETE = 5;
 	
-	var mySpectrum;
-	var myProtein;
-	var workerStatus = WORKER_AWAITING_SPECTRA;
+	var myWorkUnit; // {type:'workunit', id:0, job:0, $ipAddress:0, ms1:0, ms2:[{mz:n, intensity:n}...], peptides:[{id:1, structure:"ASDFFS"}...]}; 
+	var workerStatus = WORKER_AWAITING_WORKUNIT;
 	
-	
-
-	
-	
-	
-
-	
-
 	
 	onmessage = function (event){
 		var myObj = JSON.parse(event.data);
-		var returnObj = null;
-		var retString = "";
+		var retString;
 		switch (myObj.type)
 		{
-			case "spectrum":
-				retString = '{"type":"acknowledge","what":"spectrum"}';	
-				mySpectrum=myObj;
-				setStatus(WORKER_AWAITING_PROTEIN);
-				break;
-			case "protein":
-				retString = '{"type":"acknowledge","what":"protein"}';	
-				myProtein=myObj;
+			case "workunit":
+				retString = '{"type":"acknowledge","what":"workunit"}';
+				myWorkUnit=myObj;
 				setStatus(WORKER_PROCESSING);
 				break;
+				
 			case "confirmation":
 				retString = '{"type":"acknowledge","what":"confirmation"}';	
-				setStatus(WORKER_AWAITING_PROTEIN);
 				break;
+			
 			default:
 				retString = '{"type":"acknowledge","what":"message"}';	
 				break;
 		}
 		postMessage(retString);
-		if (workerStatus == WORKER_PROCESSING)
+		if (workerStatus === WORKER_PROCESSING)
 		{
-			doSearch();
+			doMS2Search();
+			setStatus(WORKER_COMPLETE);
 		}
 	}
 	
-	
+	//it may be useful to normalise ms2 to 0 - 100 where 100 = maximum intensity.
+	function normaliseMs2()
+	{
+		//warning I actually change mySpectrum!!!
+		var _maxIntensity = 0;
+		for (var i = 0; i < myWorkUnit.ms2.length; i++)
+		{
+			if (myWorkUnit.ms2[i].intensity > _maxIntensity)
+			{
+				_maxIntensity = myWorkUnit.ms2[i].intensity;
+			}
+		}
+		
+		for (i = 0; i < myWorkUnit.ms2.length; i++)
+		{
+			var s = myWorkUnit.ms2[i].intensity;
+			s *= 100/_maxIntensity;
+			
+			myWorkUnit.ms2[i].intensity = s;
+		}		
+	}
+
 	function setStatus(newStatus)
 	{
-		Debug("Worker Status set to " + newStatus,DEBUG_LEVEL_LOW);
 		workerStatus = newStatus;
 	}
 	 
-	function doSearch()
+	
+	// {type:'workunit', id:0, job:0, mods:=[{modtype:'fixed',modMass:0,loc:'C'}..],$ipAddress:0, ms1:0, ms2:=[{mz:n, intensity:n}...], peptides:[{id:1, structure:"ASDFFS"}...]}; 
+	function doMS2Search()
 	{
-		var resultObject = {
-			type:"result",
-			job:0,
-			ms1:0,
-			protein: 0,
-			score: 0,
-			peptides: [],
-		}
+		var resultObject = {type:"result",workunit:0,job:0,ip:0,peptides:[] };		//array of best 5 Pepdtide [{id score}]
 		
-		var pepCandidates =[];	//array of peptide indices (ie their index in protein.peptides[]
-		var myB_ions = [];		//array of B ions filled by fragment() upon successful ms1 
-		var myY_ions = [];		//array of Y ions filled by fragment() upon successful ms1
-
-		//lets set up the current resultObject properties that we know.
-		resultObject.job = myProtein.job;
-		resultObject.ms1 = mySpectrum.ms1;
-		resultObject.protein = myProtein.protein;
+			
+		var allPeptideScores=[];		//array of id, final score
+		
+		normaliseMs2();					//make ms2 intensities between 0 - 100
+		
 				
-		//ms1 search 
-		pepCandidates = ms1Search();		//returns with an array of peptide indices (ie their index in protein.peptides[]
-		if (pepCandidates.length != 0)		//we have found at least one peptide that matches the pepmass (No PTM as yet) 
-		{
-			var scores = ms2Search(pepCandidates);			//retuns an array of objects [{cumIntensity:0,b_ions:0,y_ions:0}];
-			for (i = 0; i < pepCandidates.length; i++)
+			//lets set up the current default resultObject properties that we know.
+		resultObject.job = myWorkUnit.job;
+		resultObject.workunit = myWorkUnit.id;
+		for (var p = 0; p < myWorkUnit.peptides.length; p++)
 			{
-				var peptObj = {id:0,score:0,b_count:0,y_count:0};
-				peptObj.id=myProtein.peptides[pepCandidates[i]].id;
-				peptObj.score = scores[i].cumIntensity;
-				peptObj.b_count = scores[i].b_ions;
-				peptObj.y_count=scores[i].y_ions;
-			
-				resultObject.peptides.push(peptObj);
+				var scoreObj = {id:0, score:0};
+				bscore = 0;
+				yscore = 0;
+				bcount = 0;
+				ycount = 0;
+				
+				fragment(myWorkUnit.peptides[p].structure);			//flush out myB_ions & myY_ions array.
+				
+				for (var b = 0; b < myB_ions.length; b++)
+				{
+					for (var m = 0; m < myWorkUnit.ms2.length; m++)
+					{
+						if (Math.abs(myB_ions[b]-myWorkUnit.ms2[m]['mz']) <= 0.2)
+						{
+							bcount++;
+							bscore += myWorkUnit.ms2[m]['intensity'];		
+						}
+					}
+				}
+												//check Y ions against ms2 records;								
+				for (var y = 0; y < myY_ions.length; y++)
+				{
+					for (var m = 0; m < myWorkUnit.ms2.length; m++)
+					{
+						if (Math.abs(myY_ions[y]-myWorkUnit.ms2[m]['mz']) <= 0.2)
+						{
+							ycount++;
+							yscore += myWorkUnit.ms2[m]['intensity'];		
+						}
+					}
+				}
+				
+				//important bit.... 				//score = (bscore+yscore)*(bcount+ycount);		//we will need to investigate the best scoring formula 
+				scoreObj.score = (bscore+yscore)*(bcount)*(ycount);
+				scoreObj.id = myWorkUnit.peptides[p].id;
+				allPeptideScores.push(scoreObj);
 			}
-		}
-		var retString = JSON.stringify(resultObject);
-		postMessage(retString);
-	}		
 	
-	function ms1Search(){
-		var candidates = [];
-		var myPepmass = mySpectrum.pepmass*mySpectrum.charge - (mySpectrum.charge * 1.007276);		//adjusted pepmass = ms1 pepmass * charge - charge*H+; 
-		var minPepmass = myPepmass - (myPepmass*.000010);
-		var maxPepmass = myPepmass + (myPepmass*.000010);
-		//Debug("number of peptides checked against = " +myProtein.peptides.length);
-		for (i = 0; i < myProtein.peptides.length; i++)
-		{
-			var m =myProtein.peptides[i].mass;
-			if (m >= minPepmass && m <= maxPepmass)
-			{
-				candidates.push(i);
-			}
-		}
-		return candidates;		//simple array of indexes of candidate peptides 
-	}	
-	
-	
-	
-	function ms2Search(candidates)
-	{
+		//Only wish to return the MAX_NUMBER_SCORES_RETURNED	
 		
-		var scores = [];
-		Debug("number of candidates = "+candidates.length);
-		for (j = 0; j < candidates.length; j++)
-		{
-			var scoreObj = {cumIntensity:0,b_ions:0,y_ions:0};
-			score = 0;
-			bcount = 0;
-			ycount = 0;
 			
-			Debug("peptide of protein = "+candidates[j]);
-			fragment(candidates[j]);		//flush out myB_ions & myY_ions array.
-											//score+ = intensity of a match (+-0.2Daltons)
-											//check B ions against ms2 records;
-			Debug("B ions = "+myB_ions,DEBUG_LEVEL_LOW);
-			Debug("Y ions = "+myY_ions.DEBUG_LEVEL_LOW);
-						
-			for (b = 0; b < myB_ions.length; b++)
+			allPeptideScores.sort(function(a,b){return b.score - a.score});
+			var numRes = MAX_NUMBER_SCORES_RETURNED;
+			if (myWorkUnit.peptides.length < numRes)
 			{
-				for (m = 0; m < mySpectrum.ms2.length; m++)
-				{
-					if (Math.abs(myB_ions[b]-mySpectrum.ms2[m]['mz']) <= 0.2)
-					{
-						Debug("B ion match");
-						bcount++;
-						score += mySpectrum.ms2[m]['intensity'];		
-					}
-				}
+				numRes = myWorkUnit.peptides.length;
 			}
-											//check Y ions against ms2 records;								
-			for (y = 0; y < myY_ions.length; y++)
+			
+			
+			for (r = 0; r < numRes; r++)
 			{
-				for (m = 0; m < mySpectrum.ms2.length; m++)
-				{
-					if (Math.abs(myY_ions[y]-mySpectrum.ms2[m]['mz']) <= 0.2)
-					{
-						Debug("Y ion match");
-						ycount++;
-						score += mySpectrum.ms2[m]['intensity'];		
-					}
-				}
+				resultObject.peptides.push(allPeptideScores[r])
 			}
-			Debug("score ="+score);
-			scoreObj.cumIntensity = score;
-			scoreObj.b_ions=bcount;
-			scoreObj.y_ions=ycount;
-			scores.push(scoreObj);
-		}
-		return scores;
+			var retString = JSON.stringify(resultObject);
+			console.log("job = "+resultObject.workunit);
+			postMessage(retString);
 	}
-		
 	
-
+			
+	
 			
 	var AAMass = {A:71.037114,
 				R:156.101111,
@@ -209,44 +165,59 @@
 				U:150.95363,
 				W:186.079313,
 				Y:163.063329,
-				V:99.068414
+				V:99.068414,
+				X:0
 				};
 				
-	
+	var myB_ions = [];		//array of B ions filled by fragment() upon successful ms1 
+	var myY_ions = [];		//array of Y ions filled by fragment() upon successful ms1
 	//creates arrays of B and Y ions in myB and myY of peptide[index] held in myProtein;
-	function fragment(index)				 
+	function fragment(struc)				 
 	{
-		
-		var myPeptide = myProtein.peptides[index]['structure'];
-		Debug("Fragmenting "+myPeptide+ " and id = "+myProtein.peptides[index]["id"]);
+		var myPeptide = struc;		//myProtein.peptides[index]['structure'];
 		var cm = 0;
 		myB_ions = [];
 		for (i = 0; i < myPeptide.length; i++)
 		{
-			var aa = myPeptide.charAt(i);	
+			var aa = myPeptide.charAt(i);
+			modmass = checkforPTM(aa,'b');
 			var m = AAMass[aa]; 
-			if (i == 0){	//first time through? -0H + water
+			if (i === 0){	//first time through? -0H + water
 				m += 1.007276;		//add a hydrogen
 			}
+			m +=modmass;
 			cm+=m;
 			myB_ions[i]= cm.toFixed(6);
 		}
 		cm = 0;
 		myY_ions=[];
-		for (i = myPeptide.length-1; i >= 0; i--)
+		for (var i = myPeptide.length-1; i >= 0; i--)
 		{
-			var aa = myPeptide.charAt(i);
-			var m = AAMass[aa]; 
-			if (i == (myPeptide.length-1)){	//first time through? H + water
-				m +=  18.010565+1.007276;		
+			var aay = myPeptide.charAt(i);
+			modmass = checkforPTM(aay,'y');
+			var my = AAMass[aay]; 
+			if (i === (myPeptide.length-1)){	//first time through? H + water
+				my +=  18.010565+1.007276;		
 			}
-			cm+=m;
+			my+=modmass;
+			cm+=my;
 			myY_ions[i]= cm.toFixed(6);
 		}
 	}
 	
 
-
+	function checkforPTM(aap,ion)
+	{
+		var masstoadd = 0;
+		for (var m = 0; m < myWorkUnit.mods.length; m++)
+		{
+			if (myWorkUnit.mods[m]['loc']==aap)
+			{
+				masstoadd+=myWorkUnit.mods[m]['modmass'];
+			}
+		}
+		return masstoadd;
+	}
 		
 		
 		
